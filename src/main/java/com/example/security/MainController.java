6c5d2dc69ecf7645bb5e7da8cfd8cad7bc59e500
youngho <jason.uphiller@gmail.com>
2024-02-02 16:59:40 +0900

    정보 보기 추가

--- src/main/java/com/example/security/MainController.java
+++ src/main/java/com/example/security/MainController.java
@@ -2,6 +2,8 @@ package com.example.security;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +17,20 @@ import java.util.Map;
 public class MainController {
 
     @GetMapping("/api1")
-    public ResponseEntity contents() {
+    public ResponseEntity api1() {
 
         return new ResponseEntity<>("api1 입니다.", HttpStatus.OK);
     }
 
     @GetMapping("/api2")
-    public ResponseEntity content() {
+    public ResponseEntity api2() {
 
         return new ResponseEntity<>("api2 입니다.", HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity user(@AuthenticationPrincipal UserDetails userDetails) {
+
+        return new ResponseEntity<>(userDetails.getUsername(), HttpStatus.OK);
     }
 }
